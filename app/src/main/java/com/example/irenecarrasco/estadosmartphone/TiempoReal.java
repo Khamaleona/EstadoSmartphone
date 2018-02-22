95da82d2994339dfb92ea7782e40a793f2d4db55
Javier <jagarval139@hotmail.com>
2018-02-22 12:26:45 +0100

    Creados eventos del listener hasta el de localización (no incluido).

--- app/src/main/java/com/example/irenecarrasco/estadosmartphone/TiempoReal.java
+++ app/src/main/java/com/example/irenecarrasco/estadosmartphone/TiempoReal.java
@@ -2,6 +2,11 @@ package com.example.irenecarrasco.estadosmartphone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.telephony.CellLocation;
+import android.telephony.PhoneStateListener;
+import android.telephony.ServiceState;
+import android.telephony.TelephonyManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,6 +25,7 @@ public class TiempoReal extends AppCompatActivity {
         setContentView(R.layout.activity_tiempo_real);
 
         inicializarComponentes();
+        inicializarListener();
     }
 
     private void inicializarComponentes(){
@@ -30,4 +36,85 @@ public class TiempoReal extends AppCompatActivity {
         signalLevel = findViewById(R.id.signalLevelImage);
         dataImage = findViewById(R.id.dataImage);
     }
+
+    private void inicializarListener(){
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        int eventos = PhoneStateListener.LISTEN_CALL_STATE
+                |PhoneStateListener.LISTEN_CELL_LOCATION
+                |PhoneStateListener.LISTEN_DATA_ACTIVITY
+                |PhoneStateListener.LISTEN_CALL_FORWARDING_INDICATOR
+                |PhoneStateListener.LISTEN_DATA_CONNECTION_STATE
+                |PhoneStateListener.LISTEN_SERVICE_STATE;
+        PhoneStateListener listenerTelefono = new PhoneStateListener(){
+
+            //Estado de la llamada
+            public void onCallStateChanged (int estado, String numeroEntrante){
+                String estadoTelefono = "Desconocido";
+                switch (estado){
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        estadoTelefono = "Sin actividad";
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        estadoTelefono = "Descolgado";
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        estadoTelefono = "Llamada entrante";
+                        break;
+                }
+                callStatus.setText(estadoTelefono);
+                super.onCallStateChanged(estado,numeroEntrante);
+            }
+
+            //Estado de la conexión
+            public void onDataConnectionStateChanged(int estado){
+                String estadoConexion = "Desconocido";
+                switch (estado){
+                    case TelephonyManager.DATA_DISCONNECTED:
+                        estadoConexion = "Desconectado";
+                        break;
+                    case TelephonyManager.DATA_SUSPENDED:
+                        estadoConexion = "Suspendido";
+                        break;
+                    case TelephonyManager.DATA_CONNECTING:
+                        estadoConexion = "Conectando";
+                        break;
+                    case TelephonyManager.DATA_CONNECTED:
+                        estadoConexion = "Conectado";
+                        break;
+                }
+                connectionStatus.setText(estadoConexion);
+                super.onDataConnectionStateChanged(estado);
+            }
+
+            //Estado del servicio
+            public void onServiceStateChanged(ServiceState estadoTelefono){
+                String estadoServicio = "Desconocido";
+                switch (estadoTelefono.getState()){
+                    case ServiceState.STATE_EMERGENCY_ONLY:
+                        estadoServicio = "Solo llamadas de emergencia";
+                        break;
+                    case ServiceState.STATE_IN_SERVICE:
+                        estadoServicio = "Operativo";
+                        break;
+                    case ServiceState.STATE_OUT_OF_SERVICE:
+                        estadoServicio = "Fuera de servicio";
+                        break;
+                    case ServiceState.STATE_POWER_OFF:
+                        estadoServicio = "Apagado";
+                        break;
+                }
+                serviceStatus.setText(estadoServicio);
+                super.onServiceStateChanged(estadoTelefono);
+            }
+
+            //Localización
+//            public void onCellLocationChanged(CellLocation localizacion){
+//
+//            }
+
+        };
+        tm.listen(listenerTelefono, eventos);
+    }
+
+
 }
